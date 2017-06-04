99177d7fd6b2f245f46923896e72fbeac17ed0e2
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-06-04 19:25:34 -0400

    binary tree

---
+++ src/Binary_Tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package Binary_Tree;
+
+/**
+ * Created by ZihanZhang on 17/6/4.
+ */
+
+
+public class MaximumDepthOfBinaryTree {
+
+    class TreeNode {
+        TreeNode left, right;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return 0;
+        }
+
+        if (maxDepth(root.left) > maxDepth(root.right)) {
+            return maxDepth(root.left) + 1;
+        }
+        else return maxDepth(root.right) + 1;
+    }
+}
