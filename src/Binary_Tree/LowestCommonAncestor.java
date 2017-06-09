29adbd1cea5f22237ccd07d553873cf5f296029c
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-06-08 21:46:41 -0400

    lowestcommonancestor

---
+++ src/Binary_Tree/LowestCommonAncestor.java
@@ -0,0 +1,38 @@
+package Binary_Tree;
+
+/**
+ * Created by ZihanZhang on 17/6/8.
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncester(TreeNode root, TreeNode A, TreeNode B) {
+        if ((isSubNode(root.left, A) && isSubNode(root. right, B) || (isSubNode(root.right, A) && isSubNode(root.left, B)))) {
+            return root;
+        }
+
+        if(lowestCommonAncester(root.left, A, B) != null) {
+            return lowestCommonAncester(root.left, A, B);
+        }
+
+        if(lowestCommonAncester(root.right, A, B) != null) {
+            return lowestCommonAncester(root.right, A, B);
+        }
+
+        return null;
+    }
+
+    public boolean isSubNode(TreeNode root, TreeNode sub) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root == sub) {
+            return true;
+        }
+
+        if (isSubNode(root.left, sub) || isSubNode(root.right, sub)) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/Binary_Tree/TreeNode.java
@@ -0,0 +1,8 @@
+package Binary_Tree;
+
+/**
+ * Created by ZihanZhang on 17/6/8.
+ */
+public class TreeNode {
+    TreeNode left, right;
+}
