fc4d951246412bb400a91560422c9fe341cb67b0
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-06-23 02:11:21 -0400

    minstack

---
+++ src/Data_Structure/MinStack.java
@@ -0,0 +1,49 @@
+package Data_Structure;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by ZihanZhang on 17/6/23.
+ */
+public class MinStack {
+    Stack minstack;
+
+    public MinStack() {
+        minstack = new Stack();
+    }
+
+    public void push(int number) {
+        minstack.push(number);
+    }
+
+    public int pop() {
+        int popn = (int)minstack.peek();
+        minstack.pop();
+        return popn;
+    }
+
+    public int min() {
+        ArrayList<Integer> helper = new ArrayList<Integer>();
+        int min = Integer.MAX_VALUE;
+
+        if (minstack.peek() == null) {
+            return -1;
+        }
+
+        while(minstack.peek() != null) {
+            int hmin = (int)minstack.peek();
+            helper.add(hmin);
+            if (hmin < min) {
+                min = hmin;
+            }
+            minstack.pop();
+        }
+
+        for (int i = helper.size() - 1; i >= 0; i--) {
+            minstack.push(helper.indexOf(i));
+        }
+
+        return min;
+    }
+}
