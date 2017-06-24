be535a679e98813e46b10c075315a2ce18b45afe
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-06-24 03:30:17 -0400

    1

---
+++ src/Data_Structure/LongestConsecutiveSequence.java
@@ -0,0 +1,13 @@
+package Data_Structure;
+
+/**
+ * Created by ZihanZhang on 17/6/24.
+ */
+//Hash
+public class LongestConsecutiveSequence {
+//    public int longestConsecutive(int[] num) {
+//        for (int i = 0; i < num.length; i++) {
+//
+//        }
+//    }
+}
