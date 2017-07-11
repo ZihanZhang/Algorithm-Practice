173f647f1a6490e9f794d56b9e7be10b20610e5b
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-07-11 01:22:15 -0400

    3

---
+++ src/Dynamic_Programming/MinimumAdjustmentCost.java
@@ -0,0 +1,14 @@
+package Dynamic_Programming;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ZihanZhang on 17/7/11.
+ */
+//What i can conclude from the last state is that i can infer by the last position, but the last position can
+//change, so what i have to do is put the changing stuff in array
+public class MinimumAdjustmentCost {
+//    public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+////        boolean[][] adjcost = new boolean[][]
+//    }
+}
